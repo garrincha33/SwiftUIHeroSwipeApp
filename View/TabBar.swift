ebfe4a4ec3cea131a47d1d896cf51b63eec059b1
Richard Price <mrspondooli@gmail.com>
2021-03-16 11:53:53 +0000

    InitialSetup

---
+++ View/TabBar.swift
@@ -0,0 +1,34 @@
+//
+//  TabBar.swift
+//  SwiftUIHeroSwipeApp
+//
+//  Created by Richard Price on 16/03/2021.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        TabView {
+            Text("Today").tabItem {
+                Image("today").renderingMode(.template)
+                Text("Today")
+            }
+            
+            Text("Games").tabItem {
+                Image("games").renderingMode(.template)
+                Text("Games")
+            }
+            
+            Text("Apps").tabItem {
+                Image("apps").renderingMode(.template)
+                Text("Apps")
+            }
+            
+            Text("Search").tabItem {
+                Image("search").renderingMode(.template)
+                Text("Search")
+            }
+        }
+    }
+}
